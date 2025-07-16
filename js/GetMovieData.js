54c90314299061ee650dfb0eb59d1da75c37a795
hayeonkim0418 <72956163+hayeonkim0418@users.noreply.github.com>
2025-07-16 17:03:27 +0900

    수정
    font , http > https

--- js/GetMovieData.js
+++ js/GetMovieData.js
@@ -2,7 +2,7 @@ import { MovieList } from "./MovieList.js";
 
 export const GetMovieData = async (keyword, type = "movie", year = null) => {
   try {
-    const response = await fetch(`http://www.omdbapi.com/?apikey=1633c414&s=${keyword}&type=${type}&y=${year}`);
+    const response = await fetch(`https://www.omdbapi.com/?apikey=1633c414&s=${keyword}&type=${type}&y=${year}`);
     const data = await response.json();
     console.log(data.Search);
 
