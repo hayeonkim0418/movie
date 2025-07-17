f7a6f7e01bca6d611b76a0791691011ba041ae3b
hayeonkim0418 <72956163+hayeonkim0418@users.noreply.github.com>
2025-07-17 21:14:09 +0900

    3차 업로드 - 3

--- js/ANI.js
+++ js/ANI.js
@@ -1,8 +1,43 @@
 export const ANI = () => {
   const timeline = gsap.timeline();
+  timeline.from(".movie-details .imgDiv img", {
+    // opacity: 0,
+    width: 0,
+    height: 0,
+  });
   timeline.from(".movie-details .title", {
     opacity: 0,
     y: -50,
+  });
+  timeline.from(".movie-details .basic-info strong", {
+    opacity: 0,
+    y: -50,
+  });
+  timeline.from(".movie-details .basic-info span", {
+    opacity: 0,
+    y: -50,
+  });
+  timeline.from(".movie-details .basic-info p", {
+    opacity: 0,
+    y: -50,
+    stagger: 0.3,
+  });
+  timeline.from(".movie-details .plot strong", {
+    opacity: 0,
+    y: -50,
+  });
+  timeline.from(".movie-details .plot p", {
+    opacity: 0,
+    y: -50,
+  });
+  timeline.from(".movie-details .cast strong", {
+    opacity: 0,
+    y: -50,
+  });
+  timeline.from(".movie-details .cast p", {
+    opacity: 0,
+    y: -50,
+    stagger: 0.3,
   });
 
   gsap.registerPlugin(ScrollSmoother);
